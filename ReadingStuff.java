9b30dec1e36fca2cd97e05ce5c29f9de4928dc09
chaudhry224 <46793159+chaudhry224@users.noreply.github.com>
2020-03-30 21:33:45 +1100

    Create ReadingStuff.java

---
+++ ReadingStuff.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ReadingStuff {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		float c = sc.nextFloat();
+			
+					System.out.println( s + c);
+
+		//Read something... was there something
+		//that had to be done first?
+		
+		//Concatenate the two things.
+		
+		//Print it out.
+		
+	}
+	
+}
